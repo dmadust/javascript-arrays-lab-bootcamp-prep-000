abee817d660dfb49b2fff7a293a50a183d52bddf
dmadust@gmail.com <dmadust@gmail.com>
2018-04-09 22:42:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,11 @@ function prependKitten(name) {
   var newArray = [name, ...kittens]
   return newArray
 }
+
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return kittens.slice(1)
+}
